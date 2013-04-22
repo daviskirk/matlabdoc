1206d139cf215af3c6cacedb87fb22898be478d2
Davis Kirkendall <davis.kirkendall@rwth-aachen.de>
2013-04-22 09:35:41 +0200

    Add call parser to matlabdoc_parser
    - look for function calls inside each definitions
    - add root doc_obj to support scripts (not tested)

--- matlabdoc/matlabdoc_parser.py
+++ matlabdoc/matlabdoc_parser.py
@@ -16,12 +16,26 @@ def parse_file(filename):
 
     :arg filename: path to file that will be parsed
     """
-    doc_obj_list = []  # list of dicts where information is held
-    doc_obj_dict = {}  # dict where info about the function or class is held
+
+    # Create doc_obj_dict as a dict where info about the function or class is
+    # held Initiate with root doc obj. the root doc object stores information
+    # about the file when no functions or classes have been stored yet. In a
+    # script file for example, this would be the only object added to the
+    # doc_obj_list. As soon as a new function or class definition is found, it
+    # is used to store information about it.
+    #
+    # Additionally, the doc_obj_dict also stores information about the
+    # functions that were called since the last definition. If the type is
+    # 'root', meaning that no definitions have been found yet, it stores the
+    # calls since the beginning of the script file
+    doc_obj_dict = dict(type='root', doc='', calls=[])
+    # List of dicts where information is held. Each time a function or class
+    # definition and its documentation is found it is added to the list.
+    doc_obj_list = [doc_obj_dict]
     last_line = ''
 
     file_obj = open(filename)
-    for line in file_obj:
+    for iLine, line in enumerate(file_obj):
         if doc_obj_dict:
             # look for docstring
             line = line.lstrip()
@@ -47,6 +61,7 @@ def parse_file(filename):
         # connect continued line
         if last_line:
             line = last_line + line
+            iLine = iLine - 1
         # check line continuation
         rstrip_line = line.rstrip()
         if rstrip_line.endswith('...'):
@@ -57,7 +72,16 @@ def parse_file(filename):
             last_line = ''
 
         # look for start of doc object
-        doc_obj_dict = _find_doc_obj_start(doc_obj_dict, line)
+        doc_obj_dict = _find_doc_obj_start(doc_obj_dict, line, iLine)
+
+        # look for function identifiers (function calls)
+        # add all the found calls to
+        for match in _call_re.finditer(line):
+            try:
+                doc_obj_list[-1]['calls'].add(match.group('name'))
+            except AttributeError, KeyError:
+                doc_obj_list[-1]['calls'] = set(match.group('name'))
+
     _logger.debug(pprint.pformat(doc_obj_list))
     return tuple(doc_obj_list)
 
@@ -77,7 +101,7 @@ def _add_to_docstring(doc_obj_dict, line):
     return doc_obj_dict
 
 
-def _find_doc_obj_start(doc_obj_dict, line):
+def _find_doc_obj_start(doc_obj_dict, line, iLine):
     """Find start of a doc object (classdef or function) in passed string.
     If no match is found, return an empty dict
     """
@@ -99,6 +123,14 @@ def _find_doc_obj_start(doc_obj_dict, line):
 
             # initiate docstring
             doc_obj_dict['doc'] = ''
+            # initiate calls
+            doc_obj_dict['calls'] = set()
+            # store line this was found in
+            try:
+                doc_obj_dict['line'] = iLine
+            except NameError:
+                _logger.debug('no iLine passes,  setting to None')
+                doc_obj_dict['line'] = None
             break  # out of: for key in _doc_obj_re
     return doc_obj_dict
 
@@ -121,7 +153,7 @@ def _get_doc_obj_re():
     function_re_str = function_re_str.replace(' ', space_maybe_re_str)
 
     inherit_re_str = '< (?P<supers>{varname}( & {varname})*)'
-    inherit_re_str =  inherit_re_str.format(varname=varname_re_str)
+    inherit_re_str = inherit_re_str.format(varname=varname_re_str)
     class_re_str = "(?P<sig>(?P<id>classdef) {varname} ({inherit})?)"
     class_re_str = class_re_str.format(
         inherit=inherit_re_str, varname=varname_re_str)
@@ -133,8 +165,19 @@ def _get_doc_obj_re():
     return doc_obj_re
 
 
+def _get_call_re():
+    """Get regular expression for function call.
+    """
+    varname_re_str = '[a-zA-Z]\w*'
+
+    call_re = '(?P<namespace>({varname}\.)*)(?P<name>{varname})\s?\('
+    call_re = call_re.format(varname=varname_re_str)
+    call_re = re.compile(call_re)
+    return call_re
+
 # module variables
 _doc_obj_re = _get_doc_obj_re()
+_call_re = _get_call_re()
 
 # setup module logger
 logging.basicConfig()
